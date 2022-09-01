3e37cf31d9a2f31c5f395e02ebe1fda5185cff7d
Roman <romanols1989@gmail.com>
2022-09-02 00:18:24 +0600

    программа для задачи 6

---
+++ Seminar_DZ/Zadacha_006/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите любое целое число");
+int a=Convert.ToInt32 (Console.ReadLine());
+int b=a/2;
+if (a==b*2)
+{
+    Console.WriteLine("Введенное вами число четное!");
+}
+else Console.WriteLine("Вы ввели нечетное число");

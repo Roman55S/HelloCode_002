bdde94973e88cda6d23b24413cb333c8e5da8430
Roman <romanols1989@gmail.com>
2022-09-02 01:57:17 +0600

    программа для задачи 8 не работает

---
+++ Seminar_DZ/Zadacha_008/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите любое целое число от 1 до ...");
+int N=Convert.ToInt32 (Console.ReadLine());
+int i=2;
+int j=i/2;
+while (i<=N)
+{
+    if (i==j*2)
+    {
+        Console.WriteLine (i+" ");
+        i=i+1;
+    }
+    else i=i+1;
+
+}

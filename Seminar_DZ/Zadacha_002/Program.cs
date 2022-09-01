a00d792f9e301fabb387fd0f401769c3c4d6892d
Roman <romanols1989@gmail.com>
2022-09-01 15:42:51 +0600

    программа для задачи 2

--- Seminar_DZ/Zadacha_002/Program.cs
+++ Seminar_DZ/Zadacha_002/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое целое число");
+int a=Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int b=Convert.ToInt32 (Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("Первое число больше! Это "+a);
+}
+else Console.WriteLine("Второе число больше! Это "+b);

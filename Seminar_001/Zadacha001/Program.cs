9f4f67c34bc83e01f7c0f2099a813be3f7945ce1
Roman <romanols1989@gmail.com>
2022-09-01 15:14:01 +0600

    dobavili papki

---
+++ Seminar_001/Zadacha001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите квадрат любого числа ");
+int x=Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите это число ");
+int y=Convert.ToInt32 (Console.ReadLine());
+if (x == y*y)
+{
+    Console.WriteLine ("Вы ввели верные значения! ");
+}
+else Console.WriteLine ("Вы ошиблись! ");

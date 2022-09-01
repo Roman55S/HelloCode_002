636b7c456daf3201d1be34fa7909be8d1a83b384
Roman <romanols1989@gmail.com>
2022-09-02 00:00:02 +0600

    программа для задачи 4

---
+++ Seminar_DZ/Zadacha_004/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите любое целое число");
+int a=Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите еще одно любое целое число");
+int b=Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("И еще одно целое число, пожалуйста");
+int c=Convert.ToInt32 (Console.ReadLine());
+int Max=0;
+if (a>b)
+{
+    Max=a;
+}
+else Max=b;
+if (Max>c)
+{
+    Max=Max;
+}
+else Max=c;
+Console.WriteLine("Максимальное число "+Max);

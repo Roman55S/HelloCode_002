eeb61baef8583292c248d7e844371168e5645573
Roman <romanols1989@gmail.com>
2022-09-06 22:56:40 +0600

    решение задачи  013

--- Seminar_002_DZ/Zadacha013/Program.cs
+++ Seminar_002_DZ/Zadacha013/Program.cs
@@ -1,12 +1,21 @@
 ﻿Console.WriteLine ("Введите любое целое число ");
-
-
-
-
-
-
-
-//int num = Convert.ToInt32 (Console.ReadLine());
-//int num2 = num%100/10;
-//if (num < 100) Console.WriteLine ("В введенном числе третья цифра отсутствует ");
-//else Console.WriteLine(num2);
+int num1 = Convert.ToInt32 (Console.ReadLine());
+if (num1 < 100) Console.WriteLine ("В введенном числе третья цифра отсутствует ");
+else if (num1 < 1000)
+    {
+        int num2 = num1%10;
+        Console.WriteLine($"Третья цифра в вашем числе {num2} ");
+    }
+    else
+    {
+        int num3 = num1;
+       while (num3 > 1000)
+       {
+       num3 = num3/10;
+       }
+       num3 = num3%10;
+       Console.WriteLine($"Третья цифра в вашем числе {num3} ");
+    }
+        
+        
+    

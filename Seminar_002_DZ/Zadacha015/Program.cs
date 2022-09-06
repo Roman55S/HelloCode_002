98419e8b04accaad87cbf2ee5590c86849ad8332
Roman <romanols1989@gmail.com>
2022-09-06 16:35:38 +0600

    решение задачи  015

--- Seminar_002_DZ/Zadacha015/Program.cs
+++ Seminar_002_DZ/Zadacha015/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите целое число от 1 до 7 ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num<=5) Console.WriteLine ("Этот день не выходной ");
+else Console.WriteLine ("Ура! Выходной! ");

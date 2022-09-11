bd66b5d439ac6db5aba32d53eb632b5e0ba411b8
Roman <romanols1989@gmail.com>
2022-09-11 15:25:05 +0600

    Решение задачи 019

---
+++ Seminar_003_DZ/Zadacha019/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любое пятизначное число");
+try
+{
+int x = Convert.ToInt32(Console.ReadLine());
+if (x/10000==x%10 && x/1000%10==x%100/10) Console.WriteLine("Введенное число палиндромом");
+else Console.WriteLine("Введенное число не палиндромом");
+
+}
+catch
+{
+    Console.WriteLine("Введите целое число ");
+}

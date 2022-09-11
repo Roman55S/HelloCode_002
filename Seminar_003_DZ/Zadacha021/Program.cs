b54b7e2f70e8b75c6f4c35ed199a0750bb9b2f60
Roman <romanols1989@gmail.com>
2022-09-11 18:36:44 +0600

    Решение задачи 021

---
+++ Seminar_003_DZ/Zadacha021/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите точку х: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите точку х: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+Console.WriteLine($"Расстояние в 3D = {Math.Round(d,2)}");
+Console.WriteLine($"Расстояние в 3D = {d:f3}");

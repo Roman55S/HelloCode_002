ea2efc26009ac243fcc415048322ee83e8aa32fa
Roman <romanols1989@gmail.com>
2022-09-12 23:02:48 +0600

    Решение задачи 029

---
+++ Seminar_004_DZ/Zadacha029/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран. 
+N - вводится с клавиатуры. Элементы тоже вводятся с клавиатуры.
+5
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+3
+6, 1, 33 -> [6, 1, 33]*/
+int [] AddArr(int n)
+{
+int[] arr = new int[n];
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите  элемент массива", i + 1);
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+return arr;
+}
+
+Console.WriteLine("Введите N");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] NewArray = AddArr(N);
+Console.WriteLine($"Результат: [ {String.Join("; ", NewArray)} ]");
